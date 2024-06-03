23a922d8bfca08dff9ea589afb5020042e85cf0b
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2024-06-03 13:45:32 -0600

    Fix spacer height
    This fixes the scrollbar showing up when there's space below the files.

--- src/tab.rs
+++ src/tab.rs
@@ -2079,7 +2079,7 @@ impl Tab {
                     }
                 }
                 let spacer_height = height
-                    .checked_sub(max_bottom + 2 * (space_xxs as usize))
+                    .checked_sub(max_bottom + 4 * (space_xxs as usize))
                     .unwrap_or(0);
                 if spacer_height > 0 {
                     children.push(
@@ -2449,7 +2449,7 @@ impl Tab {
         }
         //TODO: HACK If we don't reach the bottom of the view, go ahead and add a spacer to do that
         {
-            let spacer_height = size.height as i32 - y as i32;
+            let spacer_height = size.height as i32 - y as i32 - 4 * space_xxs as i32;
             if spacer_height > 0 {
                 children.push(
                     widget::container(vertical_space(Length::Fixed(spacer_height as f32))).into(),
