aa21b31484516431999aaf1025f039421295baa7
Kartonrealista <63356574+Kartonrealista@users.noreply.github.com>
2024-07-06 11:52:18 +0200

    Update key_bind.rs
    Added the classical keybind for zooming in

--- src/key_bind.rs
+++ src/key_bind.rs
@@ -55,6 +55,7 @@ pub fn key_binds() -> HashMap<KeyBind, Action> {
     bind!([Ctrl], Key::Character("q".into()), WindowClose);
     bind!([Ctrl], Key::Character("n".into()), WindowNew);
     bind!([Ctrl], Key::Character("=".into()), ZoomIn);
+    bind!([Ctrl], Key::Character("+".into()), ZoomIn);
     bind!([Ctrl], Key::Character("0".into()), ZoomDefault);
     bind!([Ctrl], Key::Character("-".into()), ZoomOut);
 
