dda636fcb616790e03ce2f02538f7d4a3edb9aae
Jeremy Soller <jackpot51@gmail.com>
2024-09-09 11:05:17 -0600

    Fix navbar open in condensed mode by default

--- src/app.rs
+++ src/app.rs
@@ -1059,9 +1059,6 @@ impl Application for App {
 
     /// Creates the application, and optionally emits command on initialize.
     fn init(mut core: Core, flags: Self::Flags) -> (Self, Command<Self::Message>) {
-        //TODO: make set_nav_bar_toggle_condensed pub
-        core.nav_bar_toggle_condensed();
-
         let app_themes = vec![fl!("match-desktop"), fl!("dark"), fl!("light")];
 
         let mut app = App {
