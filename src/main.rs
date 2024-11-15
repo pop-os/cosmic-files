b76484457f95b172f597ade441c190a679d27318
Jeremy Soller <jackpot51@gmail.com>
2024-11-15 11:10:30 -0700

    Add optional support to use jemalloc as allocator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(feature = "jemalloc")]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(feature = "jemalloc")]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     cosmic_files::main()
 }
