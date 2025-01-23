b9f43cb53ef2733e85fc4abd1331d20227ef6331
Jeremy Soller <jackpot51@gmail.com>
2025-01-23 14:01:59 -0700

    Handle org.freedesktop.FileManager1 in cosmic-files-applet

--- cosmic-files-applet/src/main.rs
+++ cosmic-files-applet/src/main.rs
@@ -1,3 +1,11 @@
+mod file_manager;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //TODO: move file manager service to its own daemon?
+    let _conn_res = zbus::blocking::connection::Builder::session()?
+        .name("org.freedesktop.FileManager1")?
+        .serve_at("/org/freedesktop/FileManager1", file_manager::FileManager)?
+        .build();
+
     cosmic_files::desktop()
 }
