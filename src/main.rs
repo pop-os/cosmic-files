ec70edfff8384eb807a702e56763ac5b80fa7fc8
Michael Aaron Murphy <michael@mmurphy.dev>
2025-02-05 12:51:18 +0100

    fix: set M_MMAP_THRESHOLD to lower memory usage significantly

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,11 @@ use tikv_jemallocator::Jemalloc;
 static GLOBAL: Jemalloc = Jemalloc;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Prevents glibc from hoarding memory via memory fragmentation.
+    #[cfg(all(not(feature = "jemalloc"), target_env = "gnu"))]
+    unsafe {
+        libc::mallopt(libc::M_MMAP_THRESHOLD, 65536);
+    }
+
     cosmic_files::main()
 }
