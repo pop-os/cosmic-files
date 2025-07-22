35a9bd5347754f1ad571fb08c09db12f60146824
Josh Megnauth <jo.sh@tutanota.com>
2025-07-22 02:52:44 -0400

    Use trash-rs' is_empty more
    Fixes two instances where enumerating trash items isn't needed.

--- src/app.rs
+++ src/app.rs
@@ -2262,14 +2262,14 @@ impl Application for App {
                 NavMenuAction::RemoveFromSidebar(entity),
             ));
         }
-        if matches!(location_opt, Some(Location::Trash)) {
-            if tab::trash_entries() > 0 {
-                items.push(cosmic::widget::menu::Item::Button(
-                    fl!("empty-trash"),
-                    None,
-                    NavMenuAction::EmptyTrash,
-                ));
-            }
+        if matches!(location_opt, Some(Location::Trash))
+            && !trash::os_limited::is_empty().unwrap_or(true)
+        {
+            items.push(cosmic::widget::menu::Item::Button(
+                fl!("empty-trash"),
+                None,
+                NavMenuAction::EmptyTrash,
+            ));
         }
 
         Some(cosmic::widget::menu::items(&HashMap::new(), items))
