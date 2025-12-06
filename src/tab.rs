5f916628c19b1645226032f141e0267df1bf0a5e
Cheong Lau <234708519+Cheong-Lau@users.noreply.github.com>
2025-12-07 09:53:22 +1000

    fix: update network path when entering uri
    closes: #1406

--- src/tab.rs
+++ src/tab.rs
@@ -1362,11 +1362,11 @@ pub struct EditLocation {
 
 impl EditLocation {
     pub fn resolve(&self) -> Option<Location> {
-        if let Location::Network(uri, _, path) = &self.location {
+        if let Location::Network(uri, ..) = &self.location {
             MOUNTERS
                 .values()
                 .find_map(|mounter| mounter.dir_info(uri))
-                .map(|(uri, display_name)| Location::Network(uri, display_name, path.clone()))
+                .map(|(uri, display_name, path_opt)| Location::Network(uri, display_name, path_opt))
         } else {
             let Some(selected) = self.selected else {
                 return Some(self.location.clone());
