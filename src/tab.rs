23b5d98dcc3fef32e9ca6b734be94834a8f7013a
Jonatan Pettersson <jonatan.pettersson@proton.me>
2026-01-16 21:54:31 +0100

    feat: set placeholder for permission dropdowns

--- src/tab.rs
+++ src/tab.rs
@@ -6445,17 +6445,6 @@ impl Tab {
 
         #[cfg(unix)]
         {
-            // The following is a bit of a hack to make the dropdown selector
-            // fill the available space when selected is None so it can be
-            // clicked on easier.
-            fn dropdown_width(use_default: bool) -> Length {
-                if use_default {
-                    Length::Shrink
-                } else {
-                    Length::Fill
-                }
-            }
-
             // Only return mode part if it's the only one
             fn selected_mode_part(mut modes: BTreeSet<u32>) -> Option<usize> {
                 match (modes.pop_first(), modes.pop_first()) {
@@ -6491,7 +6480,7 @@ impl Tab {
                                 )
                             },
                         )
-                        .width(dropdown_width(mode_part_user.is_some())),
+                        .placeholder(fl!("mixed")),
                     ),
             );
 
@@ -6511,7 +6500,7 @@ impl Tab {
                                 )
                             },
                         )
-                        .width(dropdown_width(mode_part_group.is_some())),
+                        .placeholder(fl!("mixed")),
                     ),
             );
 
@@ -6529,7 +6518,7 @@ impl Tab {
                             )
                         },
                     )
-                    .width(dropdown_width(mode_part_other.is_some())),
+                    .placeholder(fl!("mixed")),
                 ),
             );
         }
