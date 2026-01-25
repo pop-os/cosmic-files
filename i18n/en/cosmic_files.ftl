8925f07bc8a02f3f42668e273552c3a934e82ba1
orta <git@orta.io>
2026-01-25 11:29:25 +0000

    Adds a way to copy the file path of a file

--- i18n/en/cosmic_files.ftl
+++ i18n/en/cosmic_files.ftl
@@ -337,6 +337,7 @@ quit = Quit
 edit = Edit
 cut = Cut
 copy = Copy
+copy-path = Copy file path
 paste = Paste
 select-all = Select all
 
--- src/app.rs
+++ src/app.rs
@@ -145,6 +145,7 @@ pub enum Action {
     AddToSidebar,
     Compress,
     Copy,
+    CopyPath,
     Cut,
     CosmicSettingsDesktop,
     CosmicSettingsDisplays,
@@ -212,6 +213,7 @@ impl Action {
             Self::AddToSidebar => Message::AddToSidebar(entity_opt),
             Self::Compress => Message::Compress(entity_opt),
             Self::Copy => Message::Copy(entity_opt),
+            Self::CopyPath => Message::CopyPath(entity_opt),
             Self::Cut => Message::Cut(entity_opt),
             Self::CosmicSettingsDesktop => Message::CosmicSettings("desktop"),
             Self::CosmicSettingsDisplays => Message::CosmicSettings("displays"),
@@ -334,6 +336,7 @@ pub enum Message {
     Compress(Option<Entity>),
     Config(Config),
     Copy(Option<Entity>),
+    CopyPath(Option<Entity>),
     CosmicSettings(&'static str),
     Cut(Option<Entity>),
     Delete(Option<Entity>),
@@ -2671,6 +2674,15 @@ impl Application for App {
                 let paths = self.selected_paths(entity_opt);
                 let contents = ClipboardCopy::new(ClipboardKind::Copy, paths);
                 return clipboard::write_data(contents);
+            }
+            Message::CopyPath(entity_opt) => {
+                let paths = self.selected_paths(entity_opt);
+                let text = paths
+                    .into_iter()
+                    .filter_map(|p| p.to_str().map(|s| s.to_string()))
+                    .collect::<Vec<_>>()
+                    .join("\n");
+                return clipboard::write(text);
             }
             Message::Cut(entity_opt) => {
                 self.set_cut(entity_opt);
